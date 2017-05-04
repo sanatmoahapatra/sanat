2806b119d0c9518758512bcdd5a579551f67780a
sanatmohapatra <sanatmohapatra1990@gmail.com>
2017-05-04 13:11:55 +0530

    ui5_test

---
+++ WebContent/ui5_test/ui5_test.view.js
@@ -0,0 +1,24 @@
+sap.ui.jsview("ui5_test.ui5_test", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf ui5_test.ui5_test
+	*/ 
+	getControllerName : function() {
+		return "ui5_test.ui5_test";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf ui5_test.ui5_test
+	*/ 
+	createContent : function(oController) {
+ 		return new sap.m.Page({
+			title: "Title",
+			content: [
+			
+			]
+		});
+	}
+
+});
